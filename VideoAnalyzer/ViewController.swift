c1b2ec634429ae9c4af4dfee8f24934ec8793582
Hosung.Kim <hosungkim5108@gmail.com>
2024-10-28 23:02:01 +0900

    load AVURLAsset

--- VideoAnalyzer/ViewController.swift
+++ VideoAnalyzer/ViewController.swift
@@ -12,6 +12,8 @@ import PhotosUI
 class ViewController: UIViewController {
     
     @IBOutlet weak var ivTest: UIImageView!
+    private var avAsset: AVURLAsset?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,10 +21,20 @@ class ViewController: UIViewController {
 
     @IBAction func onClickAddVideo(_ sender: Any) {
         var configuration = PHPickerConfiguration()
-        configuration.filter = .images
+        configuration.filter = .videos
         let pickerViewController = PHPickerViewController(configuration: configuration)
         pickerViewController.delegate = self
         present(pickerViewController, animated: true, completion: nil)
+    }
+    
+    func temp() {
+        guard let avAsset = avAsset else { return }
+        Task {
+            print(try? await avAsset.load(.duration))
+        }
+        
+        let status = avAsset.status(of: .duration)
+        print(status)
     }
 }
 
@@ -30,16 +42,25 @@ extension ViewController: PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         picker.dismiss(animated: true)
         
-        if let itemProvider = results.first?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
-            let previousImage = ivTest.image
-            itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
-                DispatchQueue.main.async {
-                    guard let self = self, let image = image as? UIImage, self.ivTest.image == previousImage else { return }
-                    self.ivTest.image = image
-                }
-            }
+//        if let itemProvider = results.first?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+//            let previousImage = ivTest.image
+//            itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+//                DispatchQueue.main.async {
+//                    guard let self = self, let image = image as? UIImage, self.ivTest.image == previousImage else { return }
+//                    self.ivTest.image = image
+//                }
+//            }
+//        }
+        guard let itemProvider = results.first?.itemProvider, let typeIdentifier = itemProvider.registeredTypeIdentifiers.first else { return }
+        
+        if itemProvider.hasItemConformingToTypeIdentifier(typeIdentifier) {
+            itemProvider.loadFileRepresentation(forTypeIdentifier: typeIdentifier, completionHandler: { [weak self] (url, error) in
+                guard let url = url else { return }
+                self?.avAsset = AVURLAsset(url: url)
+                self?.temp()
+            })
+            
         }
+        
     }
-    
-    
 }
